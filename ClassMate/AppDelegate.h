d32243f2f258e589026135070030ee23addcbef3
Florian BUREL <florian.burel@mac.com>
2014-07-11 11:12:11 +0200

    Initial Commit

---
+++ ClassMate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ClassMate
+//
+//  Created by Florian BUREL on 11/07/2014.
+//  Copyright (c) 2014 florian burel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ClassMate/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ClassMate
+//
+//  Created by Florian BUREL on 11/07/2014.
+//  Copyright (c) 2014 florian burel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
