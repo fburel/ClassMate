f381a412704a9f2ff8cb3d0bd607a5845f395e57
Florian BUREL <florian.burel@mac.com>
2014-07-11 11:26:28 +0200

    Integration de Parse, Storyboard

---
+++ ClassMate/FormationsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FormationsTableViewController.h
+//  ClassMate
+//
+//  Created by Florian BUREL on 11/07/2014.
+//  Copyright (c) 2014 florian burel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FormationsTableViewController : UITableViewController
+
+@end
